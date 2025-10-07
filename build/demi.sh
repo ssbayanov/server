2857fe8e877839859d60b5b27e05da816226741a
Ferdinand Thiessen <opensource@fthiessen.de>
2025-10-07 17:29:12 +0200

    chore: bring back all root scripts
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- build/demi.sh
+++ build/demi.sh
@@ -3,25 +3,48 @@
 # SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
 # SPDX-License-Identifier: CC0-1.0
 
-# This is a simple helper to execute npm commands in two directories
+# This is a simple helper to execute npm COMMANDs in two directories
 # we need this as we cannot use npm workspaces as they break with 2 versions of vue.
 
-if [ "run" = "$1" ]; then
-	command=$@
-elif [ "install" = "$1" ] || [ "ci" = "$1" ]; then
-	command=$@
+COMMAND=""
+FRONTEND="$(dirname $0)/frontend"
+FRONTEND_LEGACY="$(dirname $0)/frontend-legacy"
+
+build_command() {
+	if [ "install" = "$1" ] || [ "ci" = "$1" ]; then
+		COMMAND=$@
+	elif [ "run" = "$1" ]; then
+		COMMAND="run --if-present ${@:2}"
+	else
+		COMMAND="run --if-present $@"
+	fi
+}
+
+run_parallel() {
+	npx concurrently \
+		"cd \"$FRONTEND\" && npm $COMMAND" \
+		"cd \"$FRONTEND_LEGACY\" && npm $COMMAND"
+}
+
+run_sequentially() {
+	echo -e "\e[1;34m>> Running 'npm $COMMAND' for Vue 3 based frontend\e[0m"
+	echo
+	pushd "$FRONTEND"
+	npm $COMMAND
+	popd
+
+	echo -e "\e[1;34m>> Running 'npm $COMMAND' for Vue 2 based frontend\e[0m"
+	echo
+	pushd "$FRONTEND_LEGACY"
+	npm $COMMAND
+	popd
+}
+
+
+if [ "--parallel" = "$1" ]; then
+	build_command ${@:2}
+	run_parallel
 else
-	command="run $@"
+	build_command $@
+	run_sequentially
 fi
-
-echo -e "\e[1;34m>> Running 'npm $command' for Vue 3 based frontend\e[0m"
-echo
-pushd $(dirname $0)/frontend
-npm $command
-popd
-
-echo -e "\e[1;34m>> Running 'npm $command' for Vue 2 based frontend\e[0m"
-echo
-pushd $(dirname $0)/frontend-legacy
-npm $command
-popd
